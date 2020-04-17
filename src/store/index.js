552a5d0c4cb633ddd4820953d650e3b9addeeca7
MatthewJOwens <mjowens@mail.com>
2020-04-17 13:43:31 -0600

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import router from "../router";
+import { api } from "./AxiosStore";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    profile: {},
+  },
+  mutations: {
+    setProfile(state, profile) {
+      state.profile = profile;
+    },
+  },
+  actions: {
+    setBearer({}, bearer) {
+      api.defaults.headers.authorization = bearer;
+    },
+    resetBearer() {
+      api.defaults.headers.authorization = "";
+    },
+    async getProfile({ commit }) {
+      try {
+        let res = await api.get("profile");
+        commit("setProfile", res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+});
